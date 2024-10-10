13f1342d3f98fb3ed8800c48412cb0d31f105e9d
Yang Zhi <yangzhi.see@gmail.com>
2024-10-10 23:15:36 +0800

    Update TransactionParserTest.java
    Add coverage for `clearTxn` command

--- src/test/java/spleetwaise/transaction/logic/parser/TransactionParserTest.java
+++ src/test/java/spleetwaise/transaction/logic/parser/TransactionParserTest.java
@@ -1,12 +1,14 @@
 package spleetwaise.transaction.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import spleetwaise.address.logic.parser.exceptions.ParseException;
 import spleetwaise.address.testutil.TypicalPersons;
 import spleetwaise.transaction.logic.commands.AddCommand;
+import spleetwaise.transaction.logic.commands.ClearCommand;
 import spleetwaise.transaction.logic.commands.Command;
 import spleetwaise.transaction.testutil.TransactionUtil;
 
@@ -23,6 +25,12 @@ public class TransactionParserTest {
 
         AddCommand command = (AddCommand) parser.parseCommand(TransactionUtil.getAddCommand());
         assert command != null;
+    }
+
+    @Test
+    public void parseCommand_clear() throws Exception {
+        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
+        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
     }
 
     @Test
